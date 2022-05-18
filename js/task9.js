c1e514f883c74f8d29f5c0c5f73b76dc30a3130a
Den <52295780+josserden@users.noreply.github.com>
2022-05-18 12:36:26 +0300

    add clousure

--- js/task9.js
+++ js/task9.js
@@ -17,3 +17,22 @@ const calculateAverage = (...args) => {
 console.log(calculateAverage(1, 2, 3, 4)); // 2.5
 console.log(calculateAverage(14, 8, 2)); // 8
 console.log(calculateAverage(27, 43, 2, 8, 36)); // 23.2
+
+function foo() {
+  let count = 0;
+
+  return function () {
+    return (count += 1);
+  };
+}
+
+const counter = foo();
+const counter2 = foo();
+
+console.log(counter());
+console.log(counter());
+console.log(counter());
+console.log(counter());
+console.log(counter2());
+console.log(counter2());
+console.log(counter2());
