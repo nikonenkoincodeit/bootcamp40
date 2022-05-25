62d3170b5011d8eb7556dffac32c56e99910f960
Den <52295780+josserden@users.noreply.github.com>
2022-05-25 10:33:00 +0300

    create lesson

---
+++ js/index.js
@@ -0,0 +1,28 @@
+const Transaction = {
+  DEPOSIT: 'deposit',
+  WITHDRAW: 'withdraw',
+};
+
+const account = {
+  balance: 0,
+  transactions: [],
+
+  getBalance() {
+    return this.balance;
+  },
+
+  createTransaction(amount, type) {
+    return {
+      id: this.transactions.length + 1,
+      type,
+      amount,
+    };
+  },
+
+  deposit(amount) {
+    this.balance += amount;
+
+    const newTransaction = this.createTransaction(amount, Transaction.DEPOSIT);
+    this.transactions.push(newTransaction);
+  },
+};
