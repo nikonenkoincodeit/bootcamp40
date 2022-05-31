d36cc14398aaf5ee98dd8f68162d69c1b0f17f8a
Den <52295780+josserden@users.noreply.github.com>
2022-05-31 09:08:40 +0300

    init lesson

---
+++ js/bgcolor.js
@@ -0,0 +1,5 @@
+console.log('bgcolor.js');
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
---
+++ js/data/images.js
@@ -0,0 +1,16 @@
+// low-quality image placeholders
+// https://medium.com/@imgix/lqip-your-images-for-fast-loading-2523d9ee4a62
+
+export const images = [
+  'image-1.jpeg',
+  'image-2.jpeg',
+  'image-3.jpeg',
+  'image-4.jpeg',
+  'image-5.jpeg',
+  'image-6.jpeg',
+  'image-7.jpeg',
+  'image-8.jpeg',
+  'image-9.jpeg',
+  'image-10.jpeg',
+  'image-11.jpeg',
+];
