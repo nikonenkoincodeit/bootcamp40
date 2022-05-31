2a4d6dd4355aace570c4372f5c3eb397688ae91e
Den <52295780+josserden@users.noreply.github.com>
2022-05-31 12:47:05 +0300

    finish lesson

--- js/bgcolor.js
+++ js/bgcolor.js
@@ -1,4 +1,15 @@
-console.log('bgcolor.js');
+// Variables
+const button = document.querySelector('.js-button');
+const body = document.body;
+
+// Listeners
+button.addEventListener('click', _.throttle(onBtnClick, 800));
+
+// Functions
+function onBtnClick() {
+  console.log('click');
+  body.setAttribute('style', `background-color:${getRandomHexColor()}`);
+}
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
--- js/search.js
+++ js/search.js
@@ -1,1 +1,52 @@
-console.log('search.js');
+// Imports
+import { contacts } from './data/contacts.js';
+
+// Variables
+const searchInput = document.querySelector('.js-input');
+const contactsList = document.querySelector('.js-contacts-list');
+
+// Listeners
+searchInput.addEventListener(
+  'input',
+  _.debounce(handlerInputSearch, 300, { leading: true, trailing: false }),
+);
+
+// Run Functions
+renderContacts(contacts);
+
+// Functions
+function handlerInputSearch(event) {
+  const searchValue = event.target.value.toLowerCase().trim();
+
+  if (!searchValue) return;
+
+  const filteredContacts = contacts.filter(({ name }) =>
+    name.toLowerCase().includes(searchValue),
+  );
+
+  renderContacts(filteredContacts);
+}
+
+function renderContacts(array) {
+  contactsList.innerHTML = '';
+
+  const markup = array
+    .map(
+      ({ name, rating }) => /* html */ `
+          <li
+          class="list-group-item d-flex justify-content-between align-items-center"
+          >
+              <span class="fw-bold flex-grow-1 ms-3">${name}</span>
+              <span class="badge bg-primary rounded-pill">${
+                rating ? rating : 0
+              }</span>
+        </li>`,
+    )
+    .join('');
+
+  contactsList.insertAdjacentHTML('beforeend', markup);
+}
+/*
+
+
+*/
