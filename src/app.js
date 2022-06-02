2699e446be37e8d41b82dc639c7f2910ba4b366c
Den <52295780+josserden@users.noreply.github.com>
2022-06-02 10:05:29 +0300

    init lesson

--- src/app.js
+++
@@ -1,9 +0,0 @@
-import 'lazysizes';
-
-import { renderGallery } from './js/renderGallery';
-import { initProgressBar } from './js/initProgressBar';
-import refs from './js/refs';
-import { images } from './js/data/images';
-
-renderGallery(images, refs.imageGallery);
-initProgressBar();
--- src/js/refs.js
+++
@@ -1,4 +0,0 @@
-const imageGallery = document.querySelector('.js-image-gallery');
-const progressBar = document.querySelector('.js-progress-bar');
-
-export default { imageGallery, progressBar };
