59e051b28d9574a7f71ab997ebfc7dc27110ab60
Den <52295780+josserden@users.noreply.github.com>
2022-06-07 10:37:08 +0300

    add tasks

---
+++ js/task1.js
@@ -0,0 +1,23 @@
+// setTimeout(() => {
+//   console.log('setTimeout');
+// }, 0);
+
+// console.log('console.log: 1');
+
+// Promise.resolve()
+//   .then(() => {
+//     console.log('promise: 1');
+//   })
+//   .then(() => {
+//     console.log('promise: 2');
+//   });
+
+// console.log('console.log: 2');
+
+/*
+1 - console.log('console.log: 1');
+2 - console.log('console.log: 2');
+3 - console.log('promise: 1');
+4 - console.log('promise: 2');
+5 - console.log('setTimeout');
+*/
