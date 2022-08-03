dd40b7eac3b4f98697877ecf314819644ceb137d
nikonenkoincodeit <75611895+nikonenkoincodeit@users.noreply.github.com>
2022-08-03 11:46:50 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./style.css";
+
+console.log("11111 :>> ", 11111);
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  entry: path.resolve(__dirname, "./src/index.js"),
+  output: { path: path.resolve(__dirname, "build"), filename: "main.js" },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: path.resolve(__dirname, "./src/index.html"),
+      filename: "index.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        type: "asset/resource",
+      },
+    ],
+  },
+};
