0cf757919ccd593f09c4516372d4d5377933fc1a
nikonenkoincodeit <75611895+nikonenkoincodeit@users.noreply.github.com>
2022-08-07 12:31:32 +0300

    start commit

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  entry: path.resolve(__dirname, "./src/index.js"),
+  output: { path: path.resolve(__dirname, "build"), filename: "main.js" },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: path.resolve(__dirname, "./src/template/index.html"),
+      filename: "index.html",
+    }),
+    new HTMLWebpackPlugin({
+      template: path.resolve(__dirname, "./src/template/about.html"),
+      filename: "about.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        type: "asset/resource",
+      },
+    ],
+  },
+};
