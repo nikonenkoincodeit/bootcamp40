af048748210decf78f92d5ccc336ca66c524be44
nikonenkoincodeit <75611895+nikonenkoincodeit@users.noreply.github.com>
2022-08-09 07:47:53 +0300

    start

--- src/index.js
+++ src/index.js
@@ -1,34 +1,4 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./style.css";
+import "./css/style.css";
 
-import data from "./data.json";
-
-// import { hi, name, bye } from "./say";
-
-// hi("Poly");
-// bye("Poly");
-
-const rowRef = document.querySelector(".row");
-
-const images = data.map(
-  ({ img }) => `  <div class="col">
-<div class="card">
-  <img
-    src="${require(img)}"
-    class="card-img-top"
-    alt="..."
-  />
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">
-      Some quick example text to build on the card title and make up
-      the bulk of the card's content.
-    </p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
-</div>`
-);
-
-rowRef.insertAdjacentHTML("beforeend", images.join(""));
-console.log("data :>> ", images);
+console.log("11111 :>> ", 11111);
