ba1d9b2fa54e21918b5fbd6d2d95512b43737715
nikonenkoincodeit <75611895+nikonenkoincodeit@users.noreply.github.com>
2022-10-01 10:18:53 +0300

    start commit

--- src/markup/index.js
+++
@@ -1,6 +0,0 @@
-export const createMarkup = ({ value, id, checked }) => {
-  return `<li class="item ${checked ? "checked" : ""}" data-id="${id}">
-            <p class="text">${value}</p>
-            <button class="button" type="button">x</button>
-          </li>`;
-};
