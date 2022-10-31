38d186ccba33db971ad66c3d45ba200b807766b4
nikonenkoincodeit <75611895+nikonenkoincodeit@users.noreply.github.com>
2022-10-31 11:02:37 +0200

    result

--- src/js/comments.js
+++ src/js/comments.js
@@ -1,1 +1,39 @@
-console.log("comments :>> ");
+import data from "../data/comments.json";
+import { convertMs } from "../helpers/convertMs";
+import { commentsList } from "./refs";
+
+console.log("comments :>> ", data);
+
+(function () {
+  const markup = createMarkup(data);
+  addMarkup(commentsList, markup);
+})();
+
+// {
+//     "author": "John Doe",
+//     "content": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, dolorem?",
+//     "createdAt": "2022-03-22T12:43:43.020Z"
+//   },
+
+function addMarkup(elem, markup = "") {
+  elem.innerHTML = markup;
+}
+
+function createMarkup(data = []) {
+  return data
+    .map(({ author, content, createdAt }) => {
+      const time = new Date(createdAt).getTime();
+      const { days } = convertMs(time);
+      return `      <div class="card">
+    <div class="card-body">
+      <span class="text-muted">Author</span>
+      <h5 class="card-title">${author}</h5>
+      <p class="card-text">
+        ${content}
+      </p>
+    </div>
+    <div class="card-footer text-muted">posted at ${days} days</div>
+  </div>`;
+    })
+    .join("");
+}
--- src/js/refs.js
+++ src/js/refs.js
@@ -1,3 +1,4 @@
 export const inputDate = document.querySelector(".js-input-date");
 export const tableBody = document.querySelector(".js-table-body");
+export const commentsList = document.querySelector(".js-comments-list");
 export const timerDate = document.querySelector(".js-timer");
