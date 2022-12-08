0fc04c9d8b535457c312d597590c8e3eaaa0d660
nikonenkoincodeit <75611895+nikonenkoincodeit@users.noreply.github.com>
2022-12-08 12:56:44 +0200

    result

--- js/index.js
+++ js/index.js
@@ -6,7 +6,7 @@ import { Transaction } from './options.js';
 //deposit, withdraw
 
 // Variables:
-
+const historyListRef = document.querySelector('.js-history-list');
 const liveAlertPlaceholderRef = document.querySelector(
   '.js-liveAlertPlaceholder',
 );
@@ -14,15 +14,15 @@ const totalBalanceRef = document.querySelector('.js-total-balance');
 const totalDepositRef = document.querySelector('.js-total-deposit');
 const totalWithdrawRef = document.querySelector('.js-total-withdraw');
 const formRef = document.querySelector('.js-form');
-const historyListRef = document.querySelector('.js-history-list');
-const textRef = document.querySelector('.js-text');
+
 // Listeners:
 formRef.addEventListener('submit', onSubmit);
 // Functions:
 
 function onSubmit(e) {
   e.preventDefault();
-  const { operation, amount } = this.elements;
+  const { operation, amount } = e.target.elements;
+
   const operationVal = operation.value;
   const amountVal = amount.valueAsNumber;
 
@@ -30,10 +30,10 @@ function onSubmit(e) {
     return alert('Треба заповнити всі поля!');
   }
 
-  if (amountVal > 0) {
-    account.deposit({ amount: amountVal, operationVal });
+  if (operationVal === 'deposit') {
+    account.deposit({ amount: amountVal, operation: 'deposit' });
   } else {
-    account.withdraw({ amount: amountVal, operationVal });
+    account.withdraw({ amount: amountVal, operation: 'withdraw' });
   }
 
   totalBalanceRef.textContent = account.getBalance();
@@ -43,32 +43,35 @@ function onSubmit(e) {
   totalWithdrawRef.textContent = account.getTransactionTotal(
     Transaction.WITHDRAW,
   );
+
   const alertMarkup = createAlert();
-  const liMarkup = createList(operationVal, amountVal);
+  const itemMarkup = createItem(amountVal, operationVal);
   addMarkup(liveAlertPlaceholderRef, alertMarkup);
-  addMarkup(historyListRef, liMarkup);
-  textRef.style.display = 'none';
-  this.reset();
+  addMarkup(historyListRef, itemMarkup);
+  setTimeout(() => {
+    liveAlertPlaceholderRef.innerHTML = '';
+  }, 3000);
+  console.log(account);
+  e.target.reset();
 }
 
 function createAlert() {
-  return `<div class="alert alert-success alert-dismissible fade show text-center" role="alert">
-                <div>Транзакція успішна!</div>
-                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-          </div>`;
+  return `  <div class="alert alert-success alert-dismissible fade show text-center" role="alert">
+  <div>Транзакція успішна!</div>
+  <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+</div>`;
 }
 
-function createList(type, amount) {
-  console.log('type :>> ', type);
+function createItem(amount, type) {
   return `<li
-        class="list-group-item d-flex align-items-center justify-content-between text-capitalize border border-1 rounded px-4 py-2 text-dark bg-opacity-10 mb-3 ${
-          type === 'deposit' ? 'bg-success' : 'bg-danger'
-        }">
-            <span class="fs-5">Operation ${type}</span>
-            <span class="fs-5">Amount ${amount} &#8372;</span>
-        </li>`;
+  class="list-group-item d-flex align-items-center justify-content-between text-capitalize border border-1 rounded px-4 py-2 text-dark bg-opacity-10 mb-3 ${
+    type === 'deposit' ? 'bg-success' : 'bg-danger'
+  }">
+  <span class="fs-5">Operation: ${type} </span>
+  <span class="fs-5">Amount ${amount} &#8372;</span>
+</li>`;
 }
 
-function addMarkup(elem, markup) {
-  elem.insertAdjacentHTML('beforeend', markup);
+function addMarkup(element, markup) {
+  element.insertAdjacentHTML('beforeend', markup);
 }
--- js/theory.js
+++ js/theory.js
@@ -8,56 +8,56 @@
 
 // Є три способи призначення обробників подій:
 // Атрибут HTML: onclick="...".
-// DOM-властивість: elem.onclick=function.
-// btnRef.onclick = onClick;
-// Спеціальні методи: element.addEventListener(event, handler, [options]);
-
 // function onClick() {
-//   console.log('1111 :>> ', 1111);
+//   console.log('111 :>> ', 111);
 // }
 
-// const btnRef = document.querySelector('.js-btn');
-// // btnRef.addEventListener('click', () => {
-// //   console.log('2222 :>> ', 2222);
-// // });
-// btnRef.addEventListener('click', onClick);
+// DOM-властивість: elem.onclick=function.
+
+// const btn = document.querySelector('.js-btn');
+// btn.onclick = function () {
+//   console.log('111 :>> ', 111);
+// };
+
+// Спеціальні методи: element.addEventListener(event, handler, [options]);
+// const btn = document.querySelector('.js-btn');
+
+// btn.addEventListener('click', () => {
+//   console.log('1111 :>> ', 1111);
+// });
+
 //Метод  element.addEventListener(event, handler, [options]);
 //options Додатковий об'єкт із властивостями:
 //-once: якщо true, тоді обробник буде автоматично видалено після виконання.
-// btnRef.addEventListener('click', onClick, { once: true });
+
 //-capture: фаза, на якій повинен спрацювати обробник
-// const formRef = document.querySelector('form');
-// const divRef = document.querySelector('div');
-// const pRef = document.querySelector('p');
-
-// function onClick() {
-//   console.log('1111 :>> ', 1111);
-// }
-
-// function onClick2() {
-//   console.log('22222 :>> ', 22222);
-// }
-
-// [formRef, divRef, pRef].forEach(elem => {
-//   elem.addEventListener('click', onClick, { capture: true });
-//   elem.addEventListener('click', onClick2);
-// });
 
 //-passive: якщо true, то вказує, що обробник ніколи не викличе preventDefault()
 
+// const btn = document.querySelector('.js-btn');
 // const linkRef = document.querySelector('.js-link');
 
-// function onClick(e) {
-//   e.preventDefault();
+// function foo() {
 //   console.log('1111 :>> ', 1111);
 // }
 
-// linkRef.addEventListener('click', onClick);
-// //Метод removeEventListener()
+// btn.addEventListener('click', foo);
+
+// linkRef.addEventListener(
+//   'click',
+//   e => {
+//     e.preventDefault();
+//     console.log('1111 :>> ', 1111);
+//   },
+//   { passive: true },
+// );
+
+// Метод removeEventListener()
 
 // setTimeout(() => {
-//   linkRef.removeEventListener('click', onClick);
-// }, 5000);
+//   console.log('remove :>> ');
+//   btn.removeEventListener('click', foo);
+// }, 3000);
 
 //Об'єкт події
 //e, evt або event
@@ -65,74 +65,93 @@
 //event.clientX / event.clientY - Координати курсору в момент кліка щодо вікна для подій миші.
 //event.currentTarget - елемент, на якому виконується обробник події.
 
-// const btnRef = document.querySelector('.js-btn');
+// const btn = document.querySelector('.js-btn');
 
-// function onClick(e) {
-//   // e.preventDefault();
-//   console.log(e.type);
-//   console.log(e.clientX);
-//   console.log(e.clientY);
+// function foo(e) {
 //   console.log(e.target);
 //   console.log(e.currentTarget);
 // }
 
-// btnRef.addEventListener('click', onClick);
-//Ключове слово this
+// btn.addEventListener('click', foo);
 
-// const btnRef = document.querySelector('.js-btn');
+// const listRef = document.querySelector('.js-list');
 
-// function onClick(e) {
-//   console.log(this);
-//   // console.log(e.currentTarget);
+// function foo(e) {
+//   console.log(e.target);
+//   console.log(e.currentTarget);
 // }
 
-// btnRef.addEventListener('click', onClick);
+// listRef.addEventListener('click', foo);
+
+//Ключове слово this
+
+// const listRef = document.querySelector('.js-list');
+
+// function foo(e) {
+//   console.log(e.target);
+//   console.log(e.currentTarget);
+//   console.log('this ', this);
+// }
+
+// listRef.addEventListener('click', foo);
 
 //Дії браузера за замовчуванням
-//preventDefault().
 
 // const linkRef = document.querySelector('.js-link');
 
-// function onClick(e) {
+// function foo(e) {
 //   e.preventDefault();
-//   console.log('1111 :>> ', 1111);
+//   console.log(e.target);
 // }
 
-// linkRef.addEventListener('click', onClick);
+// linkRef.addEventListener('click', foo);
+
+// const formRef = document.querySelector('.js-form');
+
+// function foo2(e) {
+//   e.preventDefault();
+//   console.log(e.target);
+// }
+
+// formRef.addEventListener('submit', foo2);
 
 //Події клавіатури keydown і keyup
 
-// document.addEventListener('keydown', event => {
-//   console.log('keydown: ', event);
+// document.addEventListener('keydown', () => {
+//   console.log('keydown :>> ');
 // });
 
-// document.addEventListener('keyup', event => {
-//   console.log('Keyup: ', event);
+// document.addEventListener('keyup', () => {
+//   console.log('keyup :>> ');
 // });
 
 //Властивості key і code
 //- key повертає символ, згенерований натисканням клавіші
 //- code повертає код фізичної клавіші на клавіатурі і не залежить від мови.
-//Властивість event.key - це символ, і він може відрізнятися. Але event.code завжди
-//буде той самий.
-// document.addEventListener('keydown', event => {
-//   // console.log('key: ', event.key);
-//   console.log('code: ', event.code);
-// });
+//Властивість event.key - це символ, і він може відрізнятися. Але event.code завжди буде той самий.
+
 // Літерні клавіші мають коди на кшталт "Key<літера>": "KeyA", "KeyB" і т.д.
 // Коди числових клавіш будуються за принципом: "Digit<число>": "Digit0", "Digit1" і т.д.
 // Код спеціальних клавіш - це їхнє ім'я: "Enter", "Backspace", "Tab" і т.д.
+
+// document.addEventListener('keydown', e => {
+//   console.log(e.key);
+// });
+
+// document.addEventListener('keydown', e => {
+//   console.log(e.code);
+// });
 
 //Клавіші-модифікатори
 //Для обробки комбінації клавіш, наприклад Ctrl + s або будь-якої іншої, на об'єкті події
 //є властивості ctrlKey, altKey, shiftkey і metaKey, що зберігають булеве значення, яке
 //сигналізує про те, чи була затиснута клавіша-модифікатор.
 
-// document.addEventListener('keydown', event => {
-//   event.preventDefault();
-//   console.log('ctrlKey: ', event.ctrlKey);
-//   console.log('code: ', event.code);
-//   if (event.ctrlKey && event.code === 'KeyR') {
+// document.addEventListener('keydown', e => {
+//   console.log('e.ctrlKey :>> ', e.ctrlKey);
+//   console.log('e.code:>> ', e.code);
+
+//   if ((e.ctrlKey && e.code === 'KeyL') || e.code === 'ControlRight') {
 //     alert('Hello world!');
 //   }
 // });
@@ -141,41 +160,45 @@
 
 //Подія submit
 
-// const formRef = document.querySelector('.js-form');
-
-// function onSubmit(e) {
+const formRef = document.querySelector('.js-form');
+// formRef.addEventListener('submit', e => {
 //   e.preventDefault();
-//   const { password, number } = this.elements;
+//   const { number, password } = e.target.elements;
+//   const numVal = number.value.trim();
+//   const pasVal = password.value.trim();
+//   console.log(numVal, pasVal);
+//   e.target.reset();
+// });
 
-// console.log('password :>> ', password);
-// console.log('number :>> ', number);
-// console.log(number.valueAsNumber);
-// console.log('this :>> ', this);
-// console.log('e.currentTarget :>> ', e.currentTarget);
-// console.log('this.elements :>> ', this.elements);
-//   const formDate = new FormData(this);
-//   const obj = {};
-//   formDate.forEach((value, key) => {
-//     obj[key] = value;
+// formRef.addEventListener('submit', e => {
+//   e.preventDefault();
+//   const data = {};
+//   const formData = new FormData(e.target);
+//   formData.forEach((value, key) => {
+//     data[key] = value;
 //   });
-//   console.log(obj);
-// }
+//   console.log('data :>> ', data);
+//   //   console.log('formData :>> ', formData);
+//   e.target.reset();
+// });
 
-// formRef.addEventListener('submit', onSubmit);
+//Подія input
+// const formData = {};
+// formRef.addEventListener('input', e => {
+//   console.log('e.type :>> ', e.type);
+//   formData[e.target.name] = e.target.value.trim();
+//   console.log('formData :>> ', formData);
+// });
 
-//Подія focus і blur
-const inputRef = document.querySelector('.js-input-number');
+//Подія focus
+// formRef.password.addEventListener('focus', () => {
+//   console.log('focus :>> ');
+// });
+//Подія change і blur
+// formRef.password.addEventListener('change', () => {
+//   console.log('change :>> ');
+// });
 
-inputRef.addEventListener('focus', () => {
-  console.log('focus :>> ');
-});
-
-inputRef.addEventListener('blur', () => {
-  console.log('blur :>> ');
-});
-
-//Подія change
-
-inputRef.addEventListener('change', () => {
-  console.log('change :>> ');
-});
+// formRef.password.addEventListener('blur', () => {
+//   console.log('blur :>> ');
+// });
