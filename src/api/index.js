4768300a1753ed6d32da0755b26d268454af1fd6
nikonenkoincodeit <75611895+nikonenkoincodeit@users.noreply.github.com>
2022-12-27 11:21:49 +0200

    add axios

--- src/api/index.js
+++ src/api/index.js
@@ -1,51 +1,133 @@
+import axios from "axios";
+axios.defaults.baseURL = "http://localhost:3000/contacts/";
+
+const xhr = new XMLHttpRequest();
 const BASE_URL = "http://localhost:3000/contacts/";
 
+// export const getData = () => {
+//   return fetch(BASE_URL).then((response) => {
+//     if (response.ok) {
+//       return response.json();
+//     }
+//     throw new Error(response.statusText);
+//   });
+// };
+
+// export const getData = async () => {
+//   try {
+//     const { data } = await axios.get();
+//     return data;
+//   } catch (error) {
+//     throw new Error(error);
+//   }
+// };
+
 export const getData = () => {
-  return fetch(BASE_URL).then((response) => {
-    if (response.ok) {
-      return response.json();
-    }
-    throw new Error(response.statusText);
+  return new Promise((resolve, reject) => {
+    xhr.open("GET", BASE_URL);
+
+    xhr.responseType = "json";
+    xhr.send();
+
+    xhr.onload = () => {
+      resolve(xhr.response);
+    };
+
+    xhr.onerror = () => {
+      reject(xhr.error);
+    };
   });
 };
 
-export const sendData = (data = {}) => {
-  return fetch(BASE_URL, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(data),
-  }).then((response) => {
-    if (response.ok) {
-      return response.json();
-    }
-    throw new Error(response.statusText);
+// export const sendData = (data = {}) => {
+//   return fetch(BASE_URL, {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//     body: JSON.stringify(data),
+//   }).then((response) => {
+//     if (response.ok) {
+//       return response.json();
+//     }
+//     throw new Error(response.statusText);
+//   });
+// };
+
+// export const sendData = (data = {}) => {
+//   return axios.post("", data).then(({ data }) => data);
+// };
+
+// export const sendData = async (payload = {}) => {
+//   try {
+//     const { data } = await axios.post("", payload);
+//     return data;
+//   } catch (error) {
+//     throw new Error(error);
+//   }
+// };
+
+export const sendData = async (payload = {}) => {
+  return new Promise((resolve, reject) => {
+    xhr.open("POST", BASE_URL);
+    xhr.responseType = "json";
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.send(JSON.stringify(payload));
+
+    xhr.onload = () => {
+      resolve(xhr.response);
+    };
+
+    xhr.onerror = () => {
+      reject(xhr.error);
+    };
   });
 };
 
-export const deleteData = (id) => {
-  return fetch(BASE_URL + id, {
-    method: "DELETE",
-  }).then((response) => {
-    if (response.ok) {
-      return response.json();
-    }
-    throw new Error(response.statusText);
-  });
+// export const deleteData = (id) => {
+//   return fetch(BASE_URL + id, {
+//     method: "DELETE",
+//   }).then((response) => {
+//     if (response.ok) {
+//       return response.json();
+//     }
+//     throw new Error(response.statusText);
+//   });
+// };
+
+export const deleteData = async (id) => {
+  try {
+    const { data } = await axios.delete(id);
+    return data;
+  } catch (error) {
+    throw new Error(error);
+  }
 };
 
-export const updateData = (id, data = {}) => {
-  return fetch(BASE_URL + id, {
-    method: "PATCH",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(data),
-  }).then((response) => {
-    if (response.ok) {
-      return response.json();
-    }
-    throw new Error(response.statusText);
-  });
+// export const updateData = (id, data = {}) => {
+//   return fetch(BASE_URL + id, {
+//     method: "PATCH",
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//     body: JSON.stringify(data),
+//   }).then((response) => {
+//     if (response.ok) {
+//       return response.json();
+//     }
+//     throw new Error(response.statusText);
+//   });
+// };
+
+// export const updateData = (id, data = {}) => {
+//   return axios.patch(id, data).then(({ data }) => data);
+// };
+
+export const updateData = async (id, payload = {}) => {
+  try {
+    const { data } = await axios.patch(id, payload);
+    return data;
+  } catch (error) {
+    throw new Error(error);
+  }
 };
--- src/main.js
+++ src/main.js
@@ -2,21 +2,35 @@ import { getData, sendData, deleteData, updateData } from "./api";
 import { createCard } from "./markup";
 import { contactRef, formRef } from "./refs";
 
-console.dir("contactRef :>> ", contactRef);
-
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/style.css";
 
 // import "./theory";
 
-getData()
-  .then((response) => {
+async function init() {
+  // getData()
+  //   .then((response) => {
+  //     const markup = createCard(response);
+  //     updateContactsList(markup);
+  //   })
+  //   .catch((error) => {
+  //     console.log("error :>> ", error);
+  //   });
+
+  // const updateContactsList = (markup = "") => {
+  //   contactRef.insertAdjacentHTML("beforeend", markup);
+  // };
+
+  try {
+    const response = await getData();
     const markup = createCard(response);
     updateContactsList(markup);
-  })
-  .catch((error) => {
+  } catch (error) {
     console.log("error :>> ", error);
-  });
+  }
+}
+
+init();
 
 const updateContactsList = (markup = "") => {
   contactRef.insertAdjacentHTML("beforeend", markup);
@@ -33,47 +47,61 @@ const createDataContact = (elements = []) => {
   return data;
 };
 
-const onSubmit = (e) => {
+const onSubmit = async (e) => {
   e.preventDefault();
 
-  const { elements } = e.target;
-  const data = createDataContact(elements);
+  try {
+    const { elements } = e.target;
 
-  sendData(data)
-    .then((response) => {
-      const markup = createCard([response]);
-      updateContactsList(markup);
-    })
-    .catch((error) => {
-      console.log("error :>> ", error);
-    })
-    .finally(() => e.target.reset());
+    const data = createDataContact(elements);
+    const response = await sendData(data);
+    const markup = createCard([response]);
+    updateContactsList(markup);
+  } catch (error) {
+    console.log("error :>> ", error);
+  } finally {
+    e.target.reset();
+  }
+
+  // sendData(data)
+  //   .then((response) => {
+  //     const markup = createCard([response]);
+  //     updateContactsList(markup);
+  //   })
+  //   .catch((error) => {
+  //     console.log("error :>> ", error);
+  //   })
+  //   .finally(() => e.target.reset());
 };
 
-const onDeleteItem = (e) => {
+const onDeleteItem = async (e) => {
   const t = e.target;
   if (!t.classList.contains("btn-close")) {
     return;
   }
-  const parent = t.closest(".js-wrap-card");
-  const cardId = parent.dataset.cardid;
-  deleteData(cardId)
-    .then((response) => {
-      console.log("response :>> ", response);
-      parent.remove();
-    })
-    .catch(console.log);
+
+  try {
+    const parent = t.closest(".js-wrap-card");
+    const cardId = parent.dataset.cardid;
+    await deleteData(cardId);
+    parent.remove();
+  } catch (error) {
+    console.log("error :>> ", error);
+  }
 };
 
-const onInput = (e) => {
+const onInput = async (e) => {
   const t = e.target;
   if (!e.target.classList.contains("fw-bold")) {
     return;
   }
-  const cardId = t.closest(".js-wrap-card").dataset.cardid;
-  updateData(cardId, { name: t.textContent })
-    .then(console.log)
-    .catch(console.log);
+
+  try {
+    const cardId = t.closest(".js-wrap-card").dataset.cardid;
+    await updateData(cardId, { name: t.textContent });
+  } catch (error) {
+    console.log("error :>> ", error);
+  }
 };
 
 formRef.addEventListener("submit", onSubmit);
